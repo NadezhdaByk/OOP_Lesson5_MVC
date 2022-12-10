0a06bd2aae4e603f05914381a30029f34e422af8
Nadezhda <nbykova79@gmail.com>
2022-12-10 20:59:03 +0300

    class

--- src/personal/model/TaskMapper.java
+++ src/personal/model/TaskMapper.java
@@ -1,16 +1,16 @@
 package personal.model;
 
-public class UserMapper {
+public class TaskMapper {
 //    JSONObject employeeDetails = new JSONObject();
 //        employeeDetails.put("firstName", "Lokesh");
 //        employeeDetails.put("lastName", "Gupta");
 
-    public String map(User user) {
-        return String.format("%s,%s,%s,%s", user.getId(), user.getFirstName(), user.getLastName(), user.getPhone());
+    public String map(Task task) {
+        return String.format("%s,%s,%s,%s,%s", task.getId(), task.getName(), task.getAutor(), task.getDateIn(), task.getPriority());
     }
 
-    public User map(String line) {
+    public Task map(String line) {
         String[] lines = line.split(",");
-        return new User(lines[0], lines[1], lines[2], lines[3]);
+        return new Task(lines[0], lines[1], lines[2], lines[3], lines[4]);
     }
 }

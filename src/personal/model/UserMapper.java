0822e61820f450a2ada888aa8782273fca0b4cf3
Nadezhda <nbykova79@gmail.com>
2022-12-11 16:35:04 +0300

    abgrade user

--- src/personal/model/UserMapper.java
+++ src/personal/model/UserMapper.java
@@ -6,11 +6,11 @@ public class UserMapper {
 //        employeeDetails.put("lastName", "Gupta");
 
     public String map(User user) {
-        return String.format("%s,%s,%s,%s", user.getId(), user.getFirstName(), user.getLastName(), user.getPhone());
+        return String.format("%s;%s;%s;%s", user.getId(), user.getFirstName(), user.getLastName(), user.getPhone());
     }
 
     public User map(String line) {
-        String[] lines = line.split(",");
+        String[] lines = line.split(";");
         return new User(lines[0], lines[1], lines[2], lines[3]);
     }
 }
